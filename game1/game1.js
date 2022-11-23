9f1fd47de155dfe2fbffc2d94f26d3a4e7ebb9be
boazfr1 <boazfr1@gmail.com>
2022-11-23 12:24:17 +0200

    add some folders

---
+++ game1/game1.js
@@ -0,0 +1,23 @@
+// check.addEventListener("click", checkIfRed);
+
+// let myDivsArr = document.querySelectorAll(".game div");
+// let fatherDiv = document.querySelector(".game");
+// let counter = 0;
+
+
+
+// function fillColor() {
+//     let rand = Math.floor(Math.random() * myDivsArr.length);
+//     while (myDivsArr[rand].className === "winner") {
+//         rand = Math.floor(Math.random() * myDivsArr.length);
+//     }
+//     myDivsArr[rand].className = "winner";
+//     counter++;
+//     console.log(counter);
+//     if (counter > 8) {
+//         return alert("game over");
+//     }
+
+// }
+
+// fatherDiv.addEventListener("click", fillColor);
