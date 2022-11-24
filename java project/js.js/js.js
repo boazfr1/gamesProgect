6da8c5ff4a0d917bcda72007c0da3fb9c9acc58d
eyal <eyalalon2000@gmail.com>
2022-11-24 11:39:58 -0500

    addend

--- java project/js.js/js.js
+++ java project/js.js/js.js
@@ -1,7 +1,7 @@
-let local = document.getElementById("input")
+let local = document.getElementById("input1")
 let form = document.getElementById("local")
 
-form.addEventListener('input', (e) => {
+form.addEventListener("input1", (e) => {
     e.preventDefault();
 });
 
@@ -13,19 +13,44 @@ local.addEventListener('click', (e) => {
     let password = document.getElementById('password');
 
 name = name.value;
-localStorage.setItem('first-name', name)
+localStorage.setItem('first-name', JSON.stringify(name))
 lastname = lastname.value;
-localStorage.setItem('last-name', lastname)
+localStorage.setItem('last-name', JSON.stringify(lastname))
 email = email.value;
-localStorage.setItem('email', email)
+localStorage.setItem('email', JSON.stringify(email))
 password = password.value;
-localStorage.setItem('password', password)
-
+localStorage.setItem('password', JSON.stringify(password))
+    
 });
 
-console.log(localStorage.getItem('first-name'))
-console.log(localStorage.getItem('last-name'))
-console.log(localStorage.getItem('password'))
+let all = JSON.parse(localStorage.getItem('first-name'))
+let all2 = JSON.parse(localStorage.getItem('last-name'))
+let all3 = JSON.parse(localStorage.getItem('password'))
+
+console.log(all, all2, all3)
+
+let fname = document.getElementById("fname");
+let lname = document.getElementById("lname");
+let pwd = document.getElementById("pwd");
+
+
+local.addEventListener('click', (e) => {
+fname = fname.value
+localStorage.setItem('fname', JSON.stringify(fname))
+lname = lname.value
+localStorage.setItem('lname', JSON.stringify(lname))
+pwd = pwd.value
+localStorage.setItem('pwd', JSON.stringify(pwd))
+
+if(fname !== all && lname !== all2){
+    Error.obj
+}
+})
+
+
+
+
+
 
 
 
